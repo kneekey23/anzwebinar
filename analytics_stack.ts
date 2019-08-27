de4654a4d4da211b92a96266c496c092147bb397
Klein <nickik@88e9fe53292e.ant.amazon.com>
2019-08-27 14:30:43 -0700

    with lambda

--- analytics_stack.ts
+++ analytics_stack.ts
@@ -4,6 +4,7 @@ import kinesis = require('@aws-cdk/aws-kinesis');
 import kinesisfirehose = require('@aws-cdk/aws-kinesisfirehose');
 import athena = require('@aws-cdk/aws-athena');
 import s3 = require('@aws-cdk/aws-s3');
+import lambda = require('@aws-cdk/aws-lambda');
 import cdk = require('@aws-cdk/cdk');
 
 class MyStack extends cdk.Stack {
@@ -112,10 +113,29 @@ SELECT * FROM userclicks.user_clicks`
             bucketName: "anzwebinar2019"
         });
 
+        const lambda4192b8c = new lambda.CfnFunction(this, 'lambda4192b8c', {
+            description: "An Amazon Kinesis Firehose stream processor that accesses the records in the input and returns them with a processing status.",
+            functionName: "KinesisDelimiter",
+            handler: "index.handler",
+            code: {
+                s3Bucket: "awslambda-us-west-2-tasks",
+                s3Key: "/snapshots/805580953652/KinesisDelimiter-6731f4f9-e721-4f7e-a6b7-dc31da65d5df",
+                s3ObjectVersion: "6VX0jYCX2nJlx3XgJFenkMcaYQbUOyJp"
+            },
+            memorySize: 128,
+            role: "arn:aws:iam::805580953652:role/MythicalMysfitsStreamingS-MysfitsClicksProcessorRo-U74OV2E30VKM",
+            runtime: "nodejs8.10",
+            timeout: 60,
+            tracingConfig: {
+                mode: "PassThrough"
+            }
+        });
+
         new cdk.Output(this, 'kinesisc5cff84Ref', { value: kinesisc5cff84.ref, disableExport: true })
         new cdk.Output(this, 'kinesis1001474Ref', { value: kinesis1001474.ref, disableExport: true })
         new cdk.Output(this, 'athena36afefaRef', { value: athena36afefa.ref, disableExport: true })
-        new cdk.Output(this, 's3dcf98edRef', { value: s3dcf98ed.ref, disableExport: true });
+        new cdk.Output(this, 's3dcf98edRef', { value: s3dcf98ed.ref, disableExport: true })
+        new cdk.Output(this, 'lambda4192b8cRef', { value: lambda4192b8c.ref, disableExport: true });
     }
 }
 
